d797188fe12b74a513266e7c8cfa19bbd092b50c
minjongbaek <minjongbaek@gmail.com>
2023-01-09 14:39:00 +0900

    feat: BrowserRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import useAxiosInterceptor from './hooks/useAxiosInterceptor';
+import { HomePage, LoginPage, ProfilePage } from './pages';
+import SignUpPage from './pages/SignUpPage';
+import PostPage from './pages/PostPage';
+import NotificationsPage from './pages/NotificationsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   useAxiosInterceptor();
 
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/posts/:postId" element={<PostPage />}></Route>
+        <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        <Route path="/notifications" element={<NotificationsPage />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
