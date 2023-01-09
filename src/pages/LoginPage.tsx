9a6c5f58b74344c34a9bf72ac7bfb2100e13a463
minjongbaek <minjongbaek@gmail.com>
2023-01-09 20:55:58 +0900

    feat: 로그인 로직 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,84 @@
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import { UserResponse } from '../types/user';
+import axiosInstance from '../utils/axios';
+import { useSetRecoilState, useRecoilState } from 'recoil';
+import { tokenState, userState } from '../recoil/atoms/user';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [user, setUser] = useRecoilState(userState);
+  const setToken = useSetRecoilState(tokenState);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      email: 'minjongbaek@gmail.com',
+      password: '',
+    },
+  });
+
+  const login = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const { data } = await axiosInstance.post<UserResponse>('/login', {
+        email,
+        password,
+      });
+      setUser(data.user);
+      setToken(data.token);
+      // TODO: 페이지 이동
+    } catch (error) {
+      console.error(error);
+      alert('입력한 정보와 일치하는 계정이 없습니다.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit(login)}>
+        <label htmlFor="email">Email</label>
+        <StyledInput
+          id="email"
+          type="email"
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+          })}
+        />
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        <label htmlFor="password">Password</label>
+        <StyledInput
+          id="password"
+          type="password"
+          {...register('password', {
+            required: '비밀번호를 입력해주세요.',
+          })}
+        />
+        {errors.password && (
+          <ErrorMessage>{errors.password.message}</ErrorMessage>
+        )}
+        <button disabled={isSubmitting}>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
+
+const StyledInput = styled.input`
+  display: block;
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+  display: block;
+`;
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,17 @@
 export interface User {
+  coverImage?: string;
+  image?: string;
+  role?: string;
+  isOnline?: boolean;
+  comments?: string[];
+  _id?: string;
   fullName?: string;
+  email?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface UserResponse {
+  user: User;
+  token: string;
 }
