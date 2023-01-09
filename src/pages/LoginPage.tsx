2d1e772ecf2c487dcd490f591b2a107a72ba660d
minjongbaek <minjongbaek@gmail.com>
2023-01-10 00:25:16 +0900

    feat: API Response 인터페이스 작성

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,10 @@
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import axiosInstance from '../utils/axios';
+import { useSetRecoilState, useRecoilState } from 'recoil';
+import { tokenState, userState } from '../recoil/atoms/user';
+import type { LoginResponse } from '../types/response';
+
 const LoginPage = () => {
   return <div>LoginPage</div>;
 };
---
+++ src/types/response.ts
@@ -0,0 +1,71 @@
+interface BasicResponse {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface UserResponse extends BasicResponse {
+  coverImage: string; // 커버 이미지
+  image: string; // 프로필 이미지
+  role: string;
+  emailVerified: Boolean; // 사용되지 않음
+  banned: boolean; // 사용되지 않음
+  isOnline: boolean;
+  posts: PostResponse[];
+  likes: LikeResponse[];
+  comments: string[];
+  followers: [];
+  following: [];
+  notifications: NotificationResponse[];
+  messages: [];
+  fullName: string;
+  email: string;
+}
+
+export interface ChannelResponse extends BasicResponse {
+  authRequired: boolean; // 사용되지 않음
+  posts: string[];
+  name: string;
+  description: string;
+}
+
+export interface PostResponse extends BasicResponse {
+  likes: LikeResponse[];
+  comments: Comment[];
+  image?: string;
+  imagePublicId?: string;
+  title: string;
+  channel: ChannelResponse;
+  author: UserResponse;
+}
+
+export interface LikeResponse extends BasicResponse {
+  user: string; // 사용자 id
+  post: string; // 포스트 id
+}
+
+export interface CommentResponse extends BasicResponse {
+  comment: string;
+  author: UserResponse;
+  post: string; // 포스트 id
+}
+
+export interface NotificationResponse extends BasicResponse {
+  seen: boolean;
+  author: UserResponse;
+  user: UserResponse | String;
+  post: string | null; // 포스트 id
+  follow?: string; // 사용자 id
+  comment?: CommentResponse;
+  message?: string; // 메시지 id
+}
+
+export interface FollowResponse extends BasicResponse {
+  user: string; // 사용자 id
+  follower: string; // 사용자 id
+}
+
+export interface LoginResponse {
+  user: UserResponse;
+  token: string;
+}
