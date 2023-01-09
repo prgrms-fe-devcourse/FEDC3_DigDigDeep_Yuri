d94536518140ba9501bdab15823ead56f4b1a033
mxx-kor <minjae7728@gmail.com>
2023-01-10 01:45:57 +0900

    feature: post get 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { PostResponse } from '../types/response';
+import axiosInstance from '../utils/axios';
 
 const HomePage = () => {
   const [search, setSearch] = useState('');
   const [select, setSelect] = useState('all');
+  const [posts, setPosts] = useState<PostResponse[]>([]);
   const navigate = useNavigate();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,6 +21,16 @@ const HomePage = () => {
     e.preventDefault();
     navigate(`/search?q=${search}&type=${select}`);
   };
+
+  const getPosts = useCallback(async () => {
+    // 10개만 offset으로 무한스크롤 구현 가능
+    const { data } = await axiosInstance.get<PostResponse[]>(`/posts?limit=10`);
+    setPosts(data);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
   return (
     <div>
@@ -38,7 +51,18 @@ const HomePage = () => {
         <input type="search" value={search} onChange={onChange} />
         <button type="submit">Search</button>
       </form>
-      {select}
+      <ul>
+        {posts.map((el) => (
+          <li key={el._id}>
+            <div>{el.title}</div>
+            <div>{el.createdAt}</div>
+            <div>{el.author.fullName}</div>
+            <div>likes: {el.likes.length}</div>
+            <div>comments: {el.comments.length}</div>
+            <button>share</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
