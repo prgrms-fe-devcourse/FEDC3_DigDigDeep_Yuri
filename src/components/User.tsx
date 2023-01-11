dbe1175ad98e5094a8a5365e4fabfe7b3b04cf9c
Jane Ro <janero95@gmail.com>
2023-01-11 15:16:49 +0900

    feat: 유저 컴포넌트 구현

---
+++ src/components/User.tsx
@@ -0,0 +1,21 @@
+import { UserResponse } from '../types/user';
+
+const User = ({
+  userInfo,
+  unfollowable,
+  ...props
+}: {
+  userInfo: UserResponse;
+  unfollowable: boolean;
+}) => {
+  return (
+    <li {...props}>
+      <img src={userInfo.image} alt={userInfo.fullName} />
+      <h3>{userInfo.fullName}</h3>
+      <button>Go To Profile</button>
+      {unfollowable && <button>unfollow</button>}
+    </li>
+  );
+};
+
+export default User;
