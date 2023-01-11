0a4dbcdfd7b73fdd4cca4fed6038d1702dbd434f
minjongbaek <minjongbaek@gmail.com>
2023-01-12 02:25:11 +0900

    feat: GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,37 @@ import {
 } from './pages';
 import { useRecoilValue } from 'recoil';
 import { tokenState } from './recoil/atoms/user';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   useAxiosInterceptor();
   const token = useRecoilValue(tokenState);
 
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route
-          path="/login"
-          element={token ? <Navigate replace to="/" /> : <LoginPage />}
-        ></Route>
-        <Route
-          path="/signup"
-          element={token ? <Navigate replace to="/" /> : <SignUpPage />}
-        ></Route>
-        <Route path="/posts/:postId" element={<PostPage />}></Route>
-        <Route path="/search" element={<SearchPage />}></Route>
-        <Route path="/profile/:userId" element={<ProfilePage />}></Route>
-        <Route path="/notifications" element={<NotificationsPage />}></Route>
-        <Route path="/edit/:postId" element={<PostEditPage />}></Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route
+            path="/login"
+            element={token ? <Navigate replace to="/" /> : <LoginPage />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={token ? <Navigate replace to="/" /> : <SignUpPage />}
+          ></Route>
+          <Route path="/posts/:postId" element={<PostPage />}></Route>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+          <Route path="/notifications" element={<NotificationsPage />}></Route>
+          <Route path="/edit/:postId" element={<PostEditPage />}></Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </>
   );
 }
 
