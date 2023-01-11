fc26f59ada3b71198e4a3bbf33d352110e89bb75
dazzel3 <aakk9350@naver.com>
2023-01-12 00:32:28 +0900

    feat: COLOR 전역 변수 추가

---
+++ src/utils/color.ts
@@ -0,0 +1,14 @@
+export const COLOR = {
+  white: '#FFFFFF',
+  orange: '#E5540A',
+  brown: '#7C5B4A',
+  date: '#B7B7B7',
+  text: '#715141',
+  lightBrown: '#927160',
+  brownGray: '#BFB0A8',
+  green: '#95C746',
+  lightGray: '#DBDBDB',
+  gray: '#EDEDED',
+  black: '#000000',
+  bgColor: '#F8F8F8',
+};
