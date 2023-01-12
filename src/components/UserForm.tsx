c8fc1bd56a42ca4a24866d0d85598a005e5c51af
minjongbaek <minjongbaek@gmail.com>
2023-01-12 20:09:29 +0900

    feat: 회원가입 페이지 퍼블리싱

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const UserForm = styled.form`
-  padding: 0 1.6rem;
   width: 100%;
   box-sizing: border-box;
   display: flex;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,11 +1,47 @@
+import styled from 'styled-components';
 import SignUpForm from '../components/SignUpForm';
+import { COLOR } from '../utils/color';
 
 const SignUpPage = () => {
   return (
-    <div>
+    <SignUpPageContainer>
+      <ImageWrapper>
+        <Image src="/image/logo/big.png" alt="logo" />
+      </ImageWrapper>
+      <StyledText>Create your account!</StyledText>
       <SignUpForm />
-    </div>
+    </SignUpPageContainer>
   );
 };
 
 export default SignUpPage;
+
+const SignUpPageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 0 1.6rem;
+  box-sizing: border-box;
+`;
+
+const ImageWrapper = styled.div`
+  margin-top: 8rem;
+  margin-bottom: 5rem;
+  text-align: center;
+`;
+
+const StyledText = styled.span`
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  letter-spacing: -0.01em;
+  color: ${COLOR.text};
+  display: block;
+  margin-bottom: 2rem;
+  padding: 0 0.8rem;
+`;
+
+const Image = styled.img``;
