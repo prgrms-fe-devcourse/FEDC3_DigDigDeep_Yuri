c9b3e783078311a0416917acb49c0e2b52623c57
minjongbaek <minjongbaek@gmail.com>
2023-01-12 15:25:27 +0900

    feat: Divider 컴포넌트 작성

---
+++ src/components/Divider.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const Divider = ({ type = 'horizontal', size = 16, ...props }) => {
+  const dividerStyle = {
+    margin: type === 'vertical' ? `0 ${size}px` : `${size}px auto`,
+  };
+  return (
+    <Line
+      {...props}
+      className={type}
+      style={{ ...dividerStyle, ...props.style }}
+    />
+  );
+};
+
+export default Divider;
+
+const Line = styled.hr`
+  border: none;
+  background-color: #dadada;
+
+  &.vertical {
+    position: relative;
+    top: -1;
+    display: inline-block;
+    width: 1px;
+    height: 13px;
+    vertical-align: middle;
+  }
+
+  &.horizontal {
+    display: block;
+    width: 100%;
+    height: 1px;
+  }
+`;
