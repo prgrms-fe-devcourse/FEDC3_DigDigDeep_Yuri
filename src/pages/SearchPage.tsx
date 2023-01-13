1b287a8d51e5fe9fa542bfb8b1cd562ee67bbd98
mxx-kor <minjae7728@gmail.com>
2023-01-13 13:35:52 +0900

    feat: 포스트만 검색 기능

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,22 +1,25 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { PostResponse } from '../types/response';
 import type { User } from '../types/user';
 import axiosInstance from '../utils/axios';
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
-  const [result, setResult] = useState<User[]>([]);
+  const [userResult, setUserResult] = useState<User[]>([]);
+  const [postResult, setPostResult] = useState<PostResponse[]>([]);
   const getSearch = useCallback(async () => {
     if (searchParams.get('type') === 'users') {
-      const { data } = await axiosInstance.get(
+      const { data } = await axiosInstance.get<User[]>(
         `/search/users/${searchParams.get('q')}`
       );
-      setResult(data);
+      setUserResult(data);
     } else {
-      const { data } = await axiosInstance.get<User[]>(
+      const { data } = await axiosInstance.get<PostResponse[]>(
         `/search/all/${searchParams.get('q')}`
       );
-      setResult(data);
+      const postOnly = data.filter((el) => 'author' in el);
+      setPostResult(postOnly);
     }
   }, [searchParams]);
 
@@ -28,9 +31,11 @@ const SearchPage = () => {
     <div>
       SearchPage, q: {searchParams.get('q')}, type: {searchParams.get('type')}
       <ul>
-        {result.map((el) => (
-          <li key={el._id}>{el.fullName}</li>
-        ))}
+        {searchParams.get('type') === 'users' ? (
+          <li>{userResult.map((el) => el.fullName)}</li>
+        ) : (
+          <li>{postResult.map((el) => el.title)}</li>
+        )}
       </ul>
     </div>
   );
