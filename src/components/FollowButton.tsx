b27c33c6367ec2425b83112ee8dcd423a458763c
Jane Ro <janero95@gmail.com>
2023-01-14 22:11:32 +0900

    feat: 팔로우 버튼 컴포넌트 추가

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+import { userState } from '../recoil/atoms/user';
+import { COLOR } from '../utils/color';
+import { createFollow, deleteFollow } from '../utils/follow';
+
+const FollowButton = () => {
+  const { userId } = useParams();
+  const [user, setUser] = useRecoilState(userState);
+  const { following } = user;
+  const [isFollowing, setIsFollowing] = useState<boolean>(false);
+  const [followId, setFollowId] = useState('');
+
+  const handleFollow = async () => {
+    if (!user) {
+      alert('로그인 후 이용해 주세요!');
+      return;
+    }
+
+    try {
+      if (!isFollowing) {
+        setIsFollowing(true);
+        const { data } = await createFollow(userId as string);
+        setUser({
+          ...user,
+          following: [...user.following, data],
+        });
+      } else {
+        setIsFollowing(false);
+        deleteFollow(followId);
+        setUser({
+          ...user,
+          following: user.following.filter((item) => item.user !== userId),
+        });
+      }
+    } catch (err) {
+      alert('Something went wrong');
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    const isFollowing = following.find((item) => item.user === userId);
+    if (isFollowing) {
+      setIsFollowing(true);
+      setFollowId(isFollowing._id);
+    }
+  }, [following, userId]);
+
+  return (
+    <StyledButton onClick={handleFollow}>
+      {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
+    </StyledButton>
+  );
+};
+
+export default FollowButton;
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  background-color: ${COLOR.orange};
+`;
