594bfb88bb6fd30c1d72f853ce0e235ad1d6d230
Jane Ro <janero95@gmail.com>
2023-01-14 22:13:41 +0900

    feat: 로그인 유저의 프로필 라우팅일 경우 핸들링

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,24 +1,28 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import { userState } from '../recoil/atoms/user';
 import { COLOR } from '../utils/color';
 
 interface TProfileButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
   userId: string;
 }
-
 const ProfileButton = ({ children, userId }: TProfileButton) => {
   const navigate = useNavigate();
+  const user = useRecoilValue(userState);
 
   const handleClick = () => {
-    navigate(`/profile/${userId}`);
+    const id = userId === user._id ? 'me' : userId;
+    navigate(`/profile/${id}`);
   };
 
   return <StyledButton onClick={handleClick}>{children}</StyledButton>;
 };
 
 export default ProfileButton;
+
 const StyledButton = styled.button`
   cursor: pointer;
   background-color: ${COLOR.green};
