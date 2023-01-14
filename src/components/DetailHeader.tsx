d50edf59efdfc1eaf5a9a1a3f7622f88f74d6dc1
dazzel3 <aakk9350@naver.com>
2023-01-14 23:57:23 +0900

    feat: DetailHeader publishing

---
+++ src/components/DetailHeader.tsx
@@ -0,0 +1,42 @@
+import Icon from './Base/Icon';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { COLOR } from '../utils/color';
+
+const DetailHeader = () => {
+  return (
+    <Container>
+      <BackHomeLink to="/">
+        <Icon name="back" size={16} />
+      </BackHomeLink>
+      <Title>알림</Title>
+    </Container>
+  );
+};
+
+export default DetailHeader;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1.5fr 1fr;
+  padding: 3rem 0;
+  width: 50%;
+  @media screen and (max-width: 767px) and (orientation: portrait) {
+    width: 86%;
+    padding: 2.4rem 0;
+  }
+`;
+
+const BackHomeLink = styled(Link)`
+  font-size: 1.6rem;
+`;
+
+const Title = styled.h3`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  letter-spacing: -0.01em;
+  color: ${COLOR.text};
+  justify-self: center;
+`;
