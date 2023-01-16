d4a3bba33344b23a37d531cf3b50454aa6a9316c
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2023-01-16 19:47:54 +0900

    fix: size, width, height 모두 적용
    Co-authored-by: MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>

--- src/components/Base/Icon.tsx
+++ src/components/Base/Icon.tsx
@@ -20,7 +20,39 @@ const Icon = ({
     height,
     ...style,
   };
+import { CSSProperties } from 'react';
+import styled from 'styled-components';
 
+interface IconProps {
+  name: string;
+  size?: number;
+  width?: number;
+  height?: number;
+  style?: CSSProperties;
+}
+
+const Icon = ({ name, size, width, height, style, ...props }: IconProps) => {
+  const shapeStyle = size
+    ? { width: size, height: size, ...style }
+    : { width: width, height: height, ...style };
+
+  return (
+    <IconContainer {...props} style={shapeStyle}>
+      <StyledIcon src={`/image/icon/${name}.png`} alt={name} />
+    </IconContainer>
+  );
+};
+
+export default Icon;
+
+const IconContainer = styled.i`
+  display: inline-block;
+`;
+
+const StyledIcon = styled.img`
+  width: inherit;
+  height: inherit;
+`;
   return (
     <IconContainer {...props} style={shapeStyle}>
       <StyledIcon src={`/image/icon/${name}.png`} alt={name} />
