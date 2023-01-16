f4f8d71263475dcd72fd45e3fbb13d85f2b4e6ea
dazzel3 <aakk9350@naver.com>
2023-01-17 00:39:16 +0900

    refactor: wip

--- src/components/PostEdit.tsx
+++ src/components/PostEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { COLOR } from '../utils/color';
 import Icon from './Base/Icon';
