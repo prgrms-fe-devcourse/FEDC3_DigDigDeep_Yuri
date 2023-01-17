9c40d8c9838756c782941782f2fbb56894ee87cd
minjongbaek <minjongbaek@gmail.com>
2023-01-18 04:33:35 +0900

    feat: UserItem 컴포넌트 작성

--- src/components/User/UserItem.tsx
+++ src/components/User/UserItem.tsx
@@ -1,33 +1,25 @@
-import { useEffect, useState, useCallback } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { FollowResponse, UserResponse } from '../../types/response';
 import { unfollow } from '../../utils/api/follow';
-import { getUserInfo } from '../../utils/api/user';
 
-interface FollowItemProps {
-  follow: FollowResponse;
-  type: 'following' | 'followers';
-  onUnfollow: () => any;
+interface UserItemProps {
+  user: UserResponse;
+  type?: 'following' | 'followers';
+  follow?: FollowResponse;
+  onUnfollow?: () => any;
 }
 
-const FollowItem = ({ follow, type, onUnfollow }: FollowItemProps) => {
+const User = ({ user, type, follow, onUnfollow }: UserItemProps) => {
   const { userId } = useParams() as { userId: string };
-  const [user, setUser] = useState<UserResponse>();
-
-  const fetchUser = useCallback(async () => {
-    const userId = type === 'following' ? follow.user : follow.follower;
-    const user = await getUserInfo(userId);
-    setUser(user);
-  }, [follow, type]);
-
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
 
   const onClick = async () => {
-    if (user?._id === follow.user) await unfollow({ followId: follow._id });
-    await onUnfollow();
+    console.log(user);
+    console.log(follow);
+    if (!user || !follow) return;
+    // if (user?._id === follow.user) await unfollow({ followId: follow._id });
+    await unfollow({ followId: follow._id });
+    onUnfollow && (await onUnfollow());
   };
 
   const isUnfollowable = () => {
@@ -37,23 +29,24 @@ const FollowItem = ({ follow, type, onUnfollow }: FollowItemProps) => {
   const render = () => {
     if (!user) return null;
     return (
-      <StyledListItem>
+      <Container>
         <h4>
           <Link to={`/profile/${user._id}`}>
             {user.fullName} | {user._id}
           </Link>
           {isUnfollowable() && <Button onClick={onClick}>X</Button>}
+          {/* <Button onClick={onClick}>X</Button> */}
         </h4>
-      </StyledListItem>
+      </Container>
     );
   };
 
   return <>{render()}</>;
 };
 
-export default FollowItem;
+export default User;
 
-const StyledListItem = styled.li`
+const Container = styled.div`
   margin: 3rem;
 `;
 
