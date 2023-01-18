4162629ee08fbe6c26272d6beadf257e614e2d9a
dazzel3 <aakk9350@naver.com>
2023-01-19 01:17:47 +0900

    feat: 404 web ver. 구현

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,62 @@
-const NotFoundPage = () => {
-  return <div>404 Not Found</div>;
-};
+import styled from 'styled-components';
+import { COLOR } from '../utils/color';
+import { useNavigate } from 'react-router-dom';
 
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Image src="/image/icon/404.png" />
+      <Title>찾을 수 없는 페이지입니다.</Title>
+      <Detail>요청하신 페이지가 사라졌거나, 잘못된 경로입니다...</Detail>
+      <Button onClick={() => navigate('/')}>홈으로 이동</Button>
+      <Background src="/image/icon/ground.png" />
+    </Container>
+  );
+};
 export default NotFoundPage;
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #9dd0ff;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 20vw;
+`;
+
+const Title = styled.h1`
+  color: ${COLOR.white};
+  font-weight: 600;
+  font-size: 3rem;
+  margin: 5rem 0 1.7rem;
+`;
+
+const Detail = styled.h2`
+  color: ${COLOR.white};
+  font-weight: 400;
+  font-size: 2rem;
+`;
+
+const Button = styled.button`
+  border-radius: 23.5px;
+  border: 4px solid ${COLOR.white};
+  color: ${COLOR.white};
+  padding: 0.8rem 4rem;
+  font-weight: 600;
+  font-size: 1.6rem;
+  margin-top: 5rem;
+`;
+
+const Background = styled.img`
+  width: 100%;
+  position: absolute;
+  left: 0;
+  bottom: -2.8rem;
+`;
