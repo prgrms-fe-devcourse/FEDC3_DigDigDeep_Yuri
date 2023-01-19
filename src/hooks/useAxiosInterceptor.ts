874339cdf1f2d25c1fd4946efd0948efcea0a001
minjongbaek <minjongbaek@gmail.com>
2023-01-19 14:13:29 +0900

    fix: 로그아웃을 해도 이전 토큰이 유지되는 이슈

--- src/hooks/useAxiosInterceptor.ts
+++ src/hooks/useAxiosInterceptor.ts
@@ -1,26 +1,29 @@
 import axiosInstance from '../utils/axios';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useRecoilValue } from 'recoil';
 import { tokenState } from '../recoil/atoms/user';
 import type { AxiosRequestConfig } from 'axios';
 const useAxiosInterceptor = () => {
   const token = useRecoilValue(tokenState);
 
-  const requestHandler = (config: AxiosRequestConfig) => {
-    if (config.headers && token) {
-      config.headers['Authorization'] = `bearer ${token}`;
-    }
-    return config;
-  };
-
-  const requestInterceptors =
-    axiosInstance.interceptors.request.use(requestHandler);
+  const requestHandler = useCallback(
+    (config: AxiosRequestConfig) => {
+      if (config.headers) {
+        config.headers['Authorization'] = '';
+        if (token) config.headers['Authorization'] = `bearer ${token}`;
+      }
+      return config;
+    },
+    [token]
+  );
 
   useEffect(() => {
+    const requestInterceptors =
+      axiosInstance.interceptors.request.use(requestHandler);
     return () => {
       axiosInstance.interceptors.request.eject(requestInterceptors);
     };
-  }, [requestInterceptors]);
+  }, [requestHandler, token]);
 };
 
 export default useAxiosInterceptor;
