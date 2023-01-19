6decaf16b85c4ae48edd922bc2670c0ae453ca63
dazzel3 <aakk9350@naver.com>
2023-01-20 02:35:42 +0900

    refactor: PostEdit CSS 반응형 수정

--- src/components/Header/DetailHeader.tsx
+++ src/components/Header/DetailHeader.tsx
@@ -53,16 +53,17 @@ export default DetailHeader;
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1.5fr 1fr;
-  padding: 2rem 0;
-  width: 50%;
-  min-width: calc(767px - 14%);
+  grid-template-columns: 1fr 2fr 1fr;
+  padding: 1.6rem 0;
+  width: 35%;
+  min-width: 350px;
   align-items: center;
   justify-items: end;
+
   @media screen and (max-width: 767px) and (orientation: portrait) {
-    width: 86%;
-    padding: 1.7rem 0;
-    min-width: 86%;
+    width: 88%;
+    min-width: 0;
+    padding: 1.4rem 0;
   }
 `;
 
