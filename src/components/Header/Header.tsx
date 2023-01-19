5bedd138156a3002a934744df4af054468cc44f4
Jane Ro <janero95@gmail.com>
2023-01-19 18:22:15 +0900

    refactor: 폰트 사이즈 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -172,7 +172,7 @@ const Header = () => {
                     <LinkButton
                       to={ROUTES.NOTIFICATION}
                       name={isSeen ? 'notification-off' : 'notification-on'}
-                      size={20}
+                      size={21}
                     />
                     {user.image ? (
                       <LinkButton
@@ -243,6 +243,7 @@ const Wrapper = styled.div`
   align-items: center;
   padding: 1.8rem 0;
   gap: 1rem;
+  margin-left: 1rem;
   @media screen and (max-width: 767px) and (orientation: portrait) {
     gap: 1.5rem;
   }
@@ -270,7 +271,6 @@ const LinkContainer = css`
   padding: 1.2rem 1.6rem;
   border-radius: 23.5px;
   border: none;
-  margin-left: 1rem;
   min-width: max-content;
   cursor: pointer;
 
