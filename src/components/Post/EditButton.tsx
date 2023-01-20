c8206d279532d821abb01d005370b9732c426f43
minjongbaek <minjongbaek@gmail.com>
2023-01-20 13:42:36 +0900

    fix: useMemo 제거

--- src/components/Post/EditButton.tsx
+++ src/components/Post/EditButton.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import styled from 'styled-components';
 import { createPost, updatePost } from '../../utils/api/post';
 import { COLOR } from '../../utils/color';
