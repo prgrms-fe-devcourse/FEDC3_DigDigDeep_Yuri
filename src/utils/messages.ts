c6dc66cf431496ba4cb90ed357d6ba5eea700c8f
minjongbaek <minjongbaek@gmail.com>
2023-02-08 01:03:03 +0900

    refector: message 관련 상수 as const 적용

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -8,12 +8,12 @@ export const ERROR_MESSAGES = {
   REQUIRE_INPUT: (type: string) => `${type} 작성해주세요.`,
   SEARCH_INPUT: '검색어를 입력해주세요.',
   MAX_SIZE_IS_10MB: '파일 크기는 10MB를 넘길 수 없습니다.',
-};
+} as const;
 
 export const CONFIRM_MESSAGES = {
   DELETE_CONFIRM: '정말로 삭제하시겠습니까?',
   LOGOUT_CONFIRM: '정말로 로그아웃 하시겠습니까?',
-};
+} as const;
 
 export const SUCCESS_MESSAGES = {
   DELETE_SUCCESS: (type: string) => `${type} 삭제되었습니다.`,
@@ -27,7 +27,7 @@ export const SUCCESS_MESSAGES = {
   UNFOLLOW_SUCCESS: '언팔로우 했습니다.',
   CREATE_COMMENT_SUCCESS: '디깅 +1',
   DELETE_COMMENT_SUCCESS: '디깅 -1',
-};
+} as const;
 
 export const FORM_RULE_MESSAGE = {
   NICKNAME_REQUIRED: '닉네임을 입력해주세요.',
@@ -40,4 +40,4 @@ export const FORM_RULE_MESSAGE = {
   EMAIL_ALREADY_IN_USE: '이미 사용중인 이메일입니다.',
   INCORRECT_LOGIN_INFO:
     '이메일 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.',
-};
+} as const;
